8a92e25944e1eed02dd950b876c703f4457e6cae
Jason Turim <jason.turim@yooclid.com>
2011-06-07 00:06:14 -0400

    Add blob storage interface

---
+++ src/Euclid.Common/Extensions/StreamExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Euclid.Common.Extensions
+{
+    public static class StreamExtensions
+    {
+        public static byte[] GetBytes(this Stream stream)
+        {
+            var bytes = new byte[stream.Length];
+
+            var pos = stream.Position;
+            stream.Seek(0, SeekOrigin.Begin);
+            stream.Read(bytes, 0, bytes.Length);
+            stream.Seek(pos, SeekOrigin.Begin);
+
+            return bytes;
+        }
+    }
+}
---
+++ src/Euclid.Common/Storage/IBlobStorage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Euclid.Common.Storage
+{
+    interface IBlobStorage
+    {
+        void Delete(string uri);
+        void Delete(Uri uri);
+        
+        bool Exists(Uri uri);
+        bool Exists(string uri);
+        
+        byte[] Get(Uri uri);
+        byte[] Get(string uri);
+        
+        Uri Put(Stream blob, string name, KeyValuePair<string, string> metadata);
+    }
+}
