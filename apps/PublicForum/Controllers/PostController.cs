ddad85ee54e6822537b2fd880188f056836fe1c7
Shawn Hinsey <smhinsey@gmail.com>
2011-07-16 21:23:27 -0400

    html-only templates for forum

---
+++ apps/PublicForum/Controllers/PostController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PublicForum.Controllers
+{
+    public class PostController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult List()
+        {
+            return View();
+        }
+
+				public ActionResult Thread()
+				{
+					return View();
+				}
+
+    }
+}
--- apps/PublicForum/Global.asax.cs
+++ apps/PublicForum/Global.asax.cs
@@ -30,7 +30,7 @@ namespace PublicForum
 				(
 				 "Default", // Route name
 				 "{controller}/{action}/{id}", // URL with parameters
-				 new {controller = "Home", action = "Index", id = UrlParameter.Optional} // Parameter defaults
+				 new {controller = "Post", action = "List", id = UrlParameter.Optional} // Parameter defaults
 				);
 		}
 	}
