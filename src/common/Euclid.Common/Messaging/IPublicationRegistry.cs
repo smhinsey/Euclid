1cb44bc5e9c6bc7790ae2e1f43f205c039e22d73
Shawn Hinsey <smhinsey@gmail.com>
2011-08-17 10:06:03 -0300

    corrected type name

--- src/common/Euclid.Common/Messaging/IPublicationRegistry.cs
+++ src/common/Euclid.Common/Messaging/IPublicationRegistry.cs
@@ -6,7 +6,7 @@ namespace Euclid.Common.Messaging
 	// an implementation to specify a concrete type for TRecordContract which can be used
 	// to create new instances. Since the declaration is otherwise unused, I'm not sure about
 	// this approach, but for now, it solves a problem that resulted in an ugly implementation
-	// for CommandPublicationRegistry
+	// for CommandRegistry
 	public interface IPublicationRegistry<out TRecord, out TRecordContract>
 		where TRecord : class, TRecordContract, IPublicationRecord
 		where TRecordContract : IPublicationRecord
