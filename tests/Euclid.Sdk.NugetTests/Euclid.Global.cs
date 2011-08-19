def9d7c4757aef71f8894da17086a51dd8ab8656
logicbomb <jason.turim@gmail.com>
2011-08-19 01:25:04 -0400

    committing euclid package directory

--- tests/Euclid.Sdk.NugetTests/Euclid.Global.cs
+++ tests/Euclid.Sdk.NugetTests/Euclid.Global.cs
@@ -5,44 +5,45 @@ using Castle.Windsor;
 using Euclid.Composites;
 using Euclid.Composites.Mvc;
 using ForumAgent.Commands;
+
 namespace Euclid.Sdk.NugetTests
 {
-    public partial class MvcApplication
-    {
-      protected void Application_Start()
-      {
-          AreaRegistration.RegisterAllAreas();
+	public partial class MvcApplication
+	{
+		protected void Application_Start()
+		{
+			AreaRegistration.RegisterAllAreas();
 
-          RegisterGlobalFilters(GlobalFilters.Filters);
+			RegisterGlobalFilters(GlobalFilters.Filters);
 
-          RegisterRoutes(RouteTable.Routes);
+			RegisterRoutes(RouteTable.Routes);
 
-          var container = new WindsorContainer();
+			var container = new WindsorContainer();
 
-          var composite = new MvcCompositeApp(container);
+			var composite = new MvcCompositeApp(container);
 
-          var euclidCompositeConfiguration = new CompositeAppSettings();
+			var euclidCompositeConfiguration = new CompositeAppSettings();
 
-          /*
+			/*
            * jt: this is how a composite developer would override the default settings for the mvc composite
            * euclidCompositeConfiguration.BlobStorage.ApplyOverride(typeof(SomeBlobStorageImplementation));
            */
 
-          composite.Configure(euclidCompositeConfiguration);
+			composite.Configure(euclidCompositeConfiguration);
 
-          /*
+			/*
           jt: this is going to be injected into composites adding this pacage
           */
-          composite.AddAgent(typeof(PublishPost).Assembly);
-            
-          // composite.RegisterInputModel(new InputToFakeCommand4Converter());
-          
+			composite.AddAgent(typeof (PublishPost).Assembly);
 
-          container.Register(Component.For<ICompositeApp>().Instance(composite));
+			// composite.RegisterInputModel(new InputToFakeCommand4Converter());
 
-          Error += composite.LogUnhandledException;
 
-          BeginRequest += composite.BeginPageRequest;
-      }
-  }
+			container.Register(Component.For<ICompositeApp>().Instance(composite));
+
+			Error += composite.LogUnhandledException;
+
+			BeginRequest += composite.BeginPageRequest;
+		}
+	}
 }
