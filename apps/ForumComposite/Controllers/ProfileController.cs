0feb53106a953bdbccdcf13e68cfc212ee8a5540
Shawn Hinsey <smhinsey@gmail.com>
2011-08-29 02:17:57 -0400

    implemented basic registration & authentication; passwords aren't even hashed yet but the basics are in place; still need to wire up usernames on posts and comments

---
+++ apps/ForumComposite/Controllers/ProfileController.cs
@@ -0,0 +1,47 @@
+﻿using System.Web.Mvc;
+using System.Web.Security;
+using ForumAgent.Queries;
+using ForumComposite.Models;
+
+namespace ForumComposite.Controllers
+{
+	public class ProfileController : Controller
+	{
+		private readonly UserQueries _userQueries;
+
+		public ProfileController(UserQueries userQueries)
+		{
+			_userQueries = userQueries;
+		}
+
+		public ActionResult SignIn()
+		{
+			return View();
+		}
+
+		public ActionResult SignOut()
+		{
+			FormsAuthentication.SignOut();
+
+			return RedirectToAction("List", "Post");
+		}
+
+		[HttpPost]
+		public ActionResult Authenticate(string username, string password)
+		{
+			if (_userQueries.Authenticate(username, password))
+			{
+				FormsAuthentication.SetAuthCookie(username, false);
+
+				return RedirectToAction("List", "Post");
+			}
+			
+			return RedirectToAction("SignIn");
+		}
+
+		public ActionResult Register()
+		{
+			return View(new RegisterUserInputModel());
+		}
+	}
+}
---
+++ apps/ForumComposite/Models/SignInInputModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ForumComposite.Models
+{
+	// SELf it would be nice to have a cleaner way of handling models that aren't mapped to commands
+	public class SignInInputModel
+	{
+		public string Username { get; set; }
+
+		public string Password { get; set; }
+	}
+}
