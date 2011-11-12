11da2d0f169aa8de46f064807840f3202e7a2046
Shawn Hinsey <smhinsey@gmail.com>
2011-11-12 14:13:49 -0500

    exposition for ARs

---
+++ tests/Euclid.Framework.TestingFakes/EventSourcing/CreatePostCommand.cs
@@ -0,0 +1,13 @@
+﻿using Euclid.Framework.Cqrs;
+
+namespace Euclid.Framework.TestingFakes.EventSourcing
+{
+	public class CreatePostCommand : DefaultCommand
+	{
+		public string AuthorUsername { get; set; }
+
+		public string Body { get; set; }
+
+		public string Title { get; set; }
+	}
+}
