2e0c4e74d08b978d6ab7cbdaa6cb93dac7599760
logicbomb <jason.turim@gmail.com>
2011-11-15 03:01:34 -0500

    fix domain entity queries

--- apps/ForumAgent/Queries/OrganizationQueries.cs
+++ apps/ForumAgent/Queries/OrganizationQueries.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Euclid.Common.Storage.NHibernate;
 using Euclid.Framework.Cqrs.NHibernate;
 using ForumAgent.Domain.Entities;
@@ -51,6 +53,30 @@ namespace ForumAgent.Queries
 			var org = session.QueryOver<OrganizationEntity>().Where(u => u.OrganizationSlug == slug).SingleOrDefault();
 
 			return org.Identifier;
+		}
+
+		new public IList<Organization> List(int offset, int pageSize)
+		{
+			var session = GetCurrentSession();
+
+			var orgs = session.QueryOver<OrganizationEntity>().Skip(offset).Take(pageSize);
+
+			return orgs.List().Select(org => new Organization
+			                                 	{
+													Created = org.Created,
+													Address = org.Address,
+													Address2 = org.Address2,
+													City = org.City,
+													Country = org.Country,
+													Identifier = org.Identifier,
+													Modified = org.Modified,
+													Name = org.OrganizationName,
+													PhoneNumber = org.PhoneNumber,
+													State = org.State,
+													WebsiteUrl = org.OrganizationUrl,
+													Zip = org.Zip,
+													Slug = org.OrganizationSlug
+												}).ToList();
 		}
 	}
 }
--- apps/ForumAgent/Queries/OrganizationUserQueries.cs
+++ apps/ForumAgent/Queries/OrganizationUserQueries.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Euclid.Common.Storage.NHibernate;
 using Euclid.Framework.Cqrs.NHibernate;
 using ForumAgent.Domain.Entities;
@@ -54,6 +55,28 @@ namespace ForumAgent.Queries
 			       	  		PasswordSalt = user.PasswordHash,
 			       	  		PasswordHash = user.PasswordSalt
 			       	  	};
+		}
+
+		public new IList<OrganizationUser> List(int offset, int pageSize)
+		{
+			var session = GetCurrentSession();
+
+			var users = session.QueryOver<OrganizationUserEntity>().Skip(offset).Take(pageSize).List();
+
+			return users.Select(user => new OrganizationUser
+			                         	{
+											Created = user.Created,
+											Email = user.Email,
+											FirstName = user.FirstName,
+											Identifier = user.Identifier,
+											LastLogin = user.LastLogin,
+											LastName = user.LastName,
+											Modified = user.Modified,
+											OrganizationIdentifier = user.OrganizationEntity.Identifier,
+											Username = user.Username,
+											PasswordSalt = user.PasswordHash,
+											PasswordHash = user.PasswordSalt
+										}).ToList();
 		}
 	}
 }
