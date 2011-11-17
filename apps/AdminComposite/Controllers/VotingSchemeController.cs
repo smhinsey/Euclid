579168ee3e2fbf927f1c17bd68a2d5a55891a802
logicbomb <jason.turim@gmail.com>
2011-11-17 14:10:08 -0500

    make voting scheme selection page consistent with the other table based forms

--- apps/AdminComposite/Controllers/VotingSchemeController.cs
+++ apps/AdminComposite/Controllers/VotingSchemeController.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Web.Mvc;
 using AdminComposite.Models;
+using Euclid.Common.Messaging;
+using ForumAgent.Commands;
 using ForumAgent.Queries;
 
 namespace AdminComposite.Controllers
@@ -8,10 +10,12 @@ namespace AdminComposite.Controllers
 	public class VotingSchemeController : Controller
 	{
 		private readonly ForumQueries _forumQueries;
+		private readonly IPublisher _publisher;
 
-		public VotingSchemeController(ForumQueries forumQueries)
+		public VotingSchemeController(ForumQueries forumQueries, IPublisher publisher)
 		{
 			_forumQueries = forumQueries;
+			_publisher = publisher;
 		}
 
 		public ActionResult List(Guid forumId)
@@ -22,6 +26,18 @@ namespace AdminComposite.Controllers
 			            		ForumIdentifier = forum.Identifier,
 								SelectedScheme = forum.NoVoting ? VotingScheme.NoVoting : VotingScheme.UpDownVoting
 			            	});
+		}
+
+		public JsonResult SetVotingScheme(Guid forumId, VotingScheme selectedScheme)
+		{
+			var publicationId = _publisher.PublishMessage(new UpdateForumVotingScheme
+			                                              	{
+			                                              		ForumIdentifier = forumId,
+			                                              		NoVoting = selectedScheme == VotingScheme.NoVoting,
+			                                              		UpDownVoting = selectedScheme == VotingScheme.UpDownVoting
+			                                              	});
+
+			return Json(new {publicationId}, JsonRequestBehavior.AllowGet);
 		}
 	}
 }
