71dc04e80a013c22b9ca1b62f2f76f36c009a991
smhinsey <smhinsey@gmail.com>
2011-11-20 17:38:11 -0500

    added a swiss blue theme

--- apps/AdminComposite/Controllers/ThemeController.cs
+++ apps/AdminComposite/Controllers/ThemeController.cs
@@ -5,15 +5,16 @@ using AdminComposite.Models;
 using Euclid.Common.Messaging;
 using ForumAgent.Commands;
 using ForumAgent.Queries;
+using ForumAgent.ReadModels;
 
 namespace AdminComposite.Controllers
 {
 	[Authorize]
 	public class ThemeController : Controller
 	{
+		private const string ThemePathFormat = "~/Content/chromatron/img/forum-themes/{0}.png";
 		private readonly ForumQueries _forumQueries;
 		private readonly IPublisher _publisher;
-		private const string ThemePathFormat = "~/Content/chromatron/img/forum-themes/{0}.png";
 
 		public ThemeController(ForumQueries forumQueries, IPublisher publisher)
 		{
@@ -23,33 +24,38 @@ namespace AdminComposite.Controllers
 
 		public ActionResult List(Guid forumId)
 		{
-			var forum = _forumQueries.FindById(forumId);
+			Forum forum = _forumQueries.FindById(forumId);
 			ViewBag.ForumName = forum.Name;
 
 			var model = new SetForumThemeInputModel
-							{
-								ForumIdentifier = forumId,
-								CurrentTheme = new Tuple<string, string>(forum.Theme, Url.Content(string.Format(ThemePathFormat, forum.Theme))),
-								AvailableThemes = new List<Tuple<string, string>>
-								                  	{
-								                  		new Tuple<string, string>("Swiss", Url.Content(string.Format(ThemePathFormat, "swiss"))),
-														new Tuple<string, string>("Test-Theme1", Url.Content(string.Format(ThemePathFormat, "test-theme1"))),
-														new Tuple<string, string>("Test-Theme2", Url.Content(string.Format(ThemePathFormat, "test-theme2")))
-								                  	}
-							};
+			            	{
+			            		ForumIdentifier = forumId,
+			            		CurrentTheme =
+			            			new Tuple<string, string>(forum.Theme, Url.Content(string.Format(ThemePathFormat, forum.Theme))),
+			            		AvailableThemes =
+			            			new List<Tuple<string, string>>
+			            				{
+			            					new Tuple<string, string>
+			            						("Swiss", Url.Content(string.Format(ThemePathFormat, "swiss"))),
+			            					new Tuple<string, string>
+			            						("Swiss-Blue", Url.Content(string.Format(ThemePathFormat, "swiss-blue"))),
+			            					new Tuple<string, string>
+			            						("Test-Theme2", Url.Content(string.Format(ThemePathFormat, "test-theme2")))
+			            				}
+			            	};
 
 			return View(model);
 		}
 
 		public JsonResult SetForumTheme(Guid forumId, string theme)
 		{
-			var publicationId = _publisher.PublishMessage(new SetForumTheme
-			{
-				ForumIdentifier = forumId,
-				ThemeName = theme
-			});
+			Guid publicationId = _publisher.PublishMessage(new SetForumTheme
+			                                               	{
+			                                               		ForumIdentifier = forumId,
+			                                               		ThemeName = theme
+			                                               	});
 
-			return Json(new { publicationId }, JsonRequestBehavior.AllowGet);
+			return Json(new {publicationId}, JsonRequestBehavior.AllowGet);
 		}
 	}
 }
