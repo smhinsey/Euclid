ba11bfd2b511121fe93b2dfd858817bfe47508f7
logicbomb <jason.turim@gmail.com>
2011-11-21 01:04:08 -0500

    edit forum settings page behaves like edit organization details

--- apps/AdminComposite/Content/chromatron/js/jquery/jquery.wysiwyg.js
+++ apps/AdminComposite/Content/chromatron/js/jquery/jquery.wysiwyg.js
@@ -384,7 +384,7 @@
 		};
 
 		this.defaults = {
-			html: '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"><html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><link rel="stylesheet" href="/css/jquery.wysiwyg.in.css"></head><body>INITIAL_CONTENT</body></html>',
+			html: '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"><html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><link rel="stylesheet" href="/content/chromatron/css/jquery.wysiwyg.in.css"></head><body>INITIAL_CONTENT</body></html>',
 			debug: false,
 			controls: {  },
 			css: {  },
@@ -397,7 +397,7 @@
 			formWidth: 600,
 			i18n: false,
 			iFrameClass: '',
-			initialContent: "<p>jQuery jWYSIWYG Editor</p>",
+			initialContent: "<p></p>",
 			maxHeight: 10000,			// see autoGrow
 			messages: {
 				nonSelection: "Select the text you wish to link"
