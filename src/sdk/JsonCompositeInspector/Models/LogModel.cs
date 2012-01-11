fef7bb9a5b4c5517d5875f8cafdabe82f7254a51
jason.turim <jason.turim@gmail.com>
2012-01-11 04:11:42 -0500

    initial log viewer

---
+++ src/sdk/JsonCompositeInspector/Models/LogModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using LoggingAgent.ReadModels;
+
+namespace JsonCompositeInspector.Models
+{
+	public class LogModel
+	{
+		public IEnumerable<string> AvailableSources { get; set; }
+
+		public IEnumerable<LogEntry> Entries { get; set; }
+
+		public string SelectedSource { get; set; }
+	}
+}
--- src/sdk/JsonCompositeInspector/Module/LogModule.cs
+++ src/sdk/JsonCompositeInspector/Module/LogModule.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Linq;
 using Euclid.Composites;
+using JsonCompositeInspector.Models;
 using LoggingAgent.Queries;
 using Nancy;
 
@@ -9,10 +12,37 @@ namespace JsonCompositeInspector.Module
 		private readonly ICompositeApp _compositeApp;
 		private readonly LogQueries _logQueries;
 
-		public LogModule(ICompositeApp compositeApp, LogQueries logQueries) : base("composite/logs")
+		public LogModule(ICompositeApp compositeApp, LogQueries logQueries)
+			: base("composite/logs")
 		{
 			_compositeApp = compositeApp;
 			_logQueries = logQueries;
+
+			Get["/"] = p =>
+							{
+								var filterBy = (string)p.filterBy ?? string.Empty;
+								var pageSize = p.pageSize != null ? (int)p.pageSize : 25;
+								var offset = p.offset != null ? (int)p.offset : 0;
+
+								var startDate = DateTime.Now.AddMinutes(-15);
+								var endDate = DateTime.Now;
+								var entries = _logQueries.FindByCreationDate(startDate, endDate);
+								var sources = entries.Select(e => e.LoggingSource).Distinct();
+
+								if (!string.IsNullOrEmpty(filterBy))
+								{
+									entries = entries.Where(e => e.LoggingSource == filterBy).ToList();
+								}
+
+								var model = new LogModel
+												{
+													AvailableSources = sources,
+													Entries = entries.Skip(offset * pageSize).Take(pageSize).OrderByDescending(e => e.Created),
+													SelectedSource = filterBy
+												};
+
+								return View["logs.cshtml", model];
+							};
 		}
 	}
 }
