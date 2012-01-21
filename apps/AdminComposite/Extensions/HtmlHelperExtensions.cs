da2f910842f8e96df3525af5f21464921a391025
jason.turim <jason.turim@gmail.com>
2012-01-21 09:10:50 -0500

    added comment to html helpers

--- apps/AdminComposite/Extensions/HtmlHelperExtensions.cs
+++ apps/AdminComposite/Extensions/HtmlHelperExtensions.cs
@@ -5,6 +5,13 @@ namespace AdminComposite.Extensions
 {
 	public static class HtmlHelperExtensions
 	{
+		/// <summary>
+		/// returns true if the current route matches the speceified controller & action
+		/// </summary>
+		/// <param name="helper">html helper</param>
+		/// <param name="expectedController">the name of the controller to check</param>
+		/// <param name="expectedAction">the name of the action to check</param>
+		/// <returns>boolean</returns>
 		public static bool AreControllerAndActionAreCurrent(
 			this HtmlHelper helper, string expectedController, string expectedAction)
 		{
@@ -17,20 +24,44 @@ namespace AdminComposite.Extensions
 			         && expectedController.Equals(controller, StringComparison.OrdinalIgnoreCase)));
 		}
 
+		/// <summary>
+		/// returns a class if the current route matches the specified controller and action, otherwise string.Empty
+		/// </summary>
+		/// <param name="helper">the html helper object</param>
+		/// <param name="performCheck">whether to perform the check or not</param>
+		/// <param name="expectedController">name of controller to check</param>
+		/// <param name="expectedAction">name of action to check</param>
+		/// <param name="className">class to return</param>
+		/// <returns></returns>
 		public static string GetClassWhenControllerAndActionAreCurrent(
-			this HtmlHelper helper, bool additional, string expectedController, string expectedAction, string className)
+			this HtmlHelper helper, bool performCheck, string expectedController, string expectedAction, string className)
 		{
-			return (additional && helper.AreControllerAndActionAreCurrent(expectedController, expectedAction))
+			return (helper.AreControllerAndActionAreCurrent(expectedController, expectedAction))
 			       	? string.Format(@"class={0}", className)
 			       	: string.Empty;
 		}
 
+		/// <summary>
+		/// returns a class if the current route matches the specified controller and action, otherwise string.Empty
+		/// </summary>
+		/// <param name="helper">the html helper object</param>
+		/// <param name="expectedController">name of controller to check</param>
+		/// <param name="expectedAction">name of action to check</param>
+		/// <param name="className">class to return</param>
+		/// <returns></returns>
 		public static string GetClassWhenControllerAndActionAreCurrent(
 			this HtmlHelper helper, string expectedController, string expectedAction, string className)
 		{
 			return helper.GetClassWhenControllerAndActionAreCurrent(true, expectedController, expectedAction, className);
 		}
 
+		/// <summary>
+		/// returns a class if the current route matches the specified controller, otherwise string.Empty
+		/// </summary>
+		/// <param name="helper">the html helper object</param>
+		/// <param name="expectedController">name of controller to check</param>
+		/// <param name="className">class to return</param>
+		/// <returns></returns>
 		public static string GetClassWhenControllerIsCurrent(
 			this HtmlHelper helper, string expectedController, string className)
 		{
