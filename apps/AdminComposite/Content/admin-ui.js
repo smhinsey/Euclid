63c0617d068b5db2a42462496789b0785cfd6368
jason.turim <jason.turim@gmail.com>
2012-01-22 05:19:00 -0500

    fix adding content

--- apps/AdminComposite/Content/admin-ui.js
+++ apps/AdminComposite/Content/admin-ui.js
@@ -1,38 +1,4 @@
-﻿$(document).ready(function () {
-	$(".confirmation-dialog").live("click", function () {
-		var msg = $(this).attr("data-confirmation-message");
-		var confirmFunction = $(this).attr("data-confirm-function");
-		var itemId = $(this).attr("data-item-id");
-		var override = $(this).attr("data-override");
-
-		if (override == true) return true;
-
-		if (isNullOrEmpty(msg) || isNullOrEmpty(confirmFunction) || isNullOrEmpty(itemId)) {
-			$("<div></div>").load("/Dashboard/GetConfirmationMessageAttributesMissingMessage").modal();
-		} else {
-			$("<div></div>")
-				.load("/Dashboard/GetConfirmationMessage?message=" + $.URLEncode(msg))
-				.modal({
-					onShow: function (dialog) {
-						var modal = this;
-
-						// if the user clicks "yes"
-						$('.yes', dialog.data[0]).live('click', function () {
-							// call the callback
-							eval(confirmFunction)(itemId);
-
-							// close the dialog
-							modal.close(); // or $.modal.close();
-						});
-					}
-				});
-		}
-
-		return false;
-	});
-});
-
-function setForumUrl(host, orgSlug, forumSlug) {
+﻿function setForumUrl(host, orgSlug, forumSlug) {
 	var url = "http://" + host + "/org/" + orgSlug + "/forum/" + forumSlug;
 
 	$("#forum-url").html(url);
