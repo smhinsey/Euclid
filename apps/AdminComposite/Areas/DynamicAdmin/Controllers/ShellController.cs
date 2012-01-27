a51149432cb45ddc72b0e30c135b63564a51beb6
Shawn Hinsey <smhinsey@gmail.com>
2012-01-27 18:55:32 -0500

    initial pieces of dynamic admin shell

---
+++ apps/AdminComposite/Areas/DynamicAdmin/Controllers/ShellController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace AdminComposite.Areas.DynamicAdmin.Controllers
+{
+	public class ShellController : Controller
+	{
+		public ActionResult Index()
+		{
+			return View();
+		}
+	}
+}
---
+++ apps/AdminComposite/Areas/DynamicAdmin/DynamicAdminAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace AdminComposite.Areas.DynamicAdmin
+{
+	public class DynamicAdminAreaRegistration : AreaRegistration
+	{
+		public override string AreaName
+		{
+			get
+			{
+				return "DynamicAdmin";
+			}
+		}
+
+		public override void RegisterArea(AreaRegistrationContext context)
+		{
+			context.MapRoute(
+					"DynamicAdmin_default",
+					"DynamicAdmin/{controller}/{action}/{id}",
+					new { action = "Index", id = UrlParameter.Optional }
+			);
+		}
+	}
+}
