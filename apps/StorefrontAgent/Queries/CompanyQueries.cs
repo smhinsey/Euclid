00477162f0dd79c95f9147c41319579858fb8d08
Shawn Hinsey <smhinsey@gmail.com>
2012-02-01 17:14:22 -0500

    added skeleton of storefront agent

---
+++ apps/StorefrontAgent/Queries/CompanyQueries.cs
@@ -0,0 +1,14 @@
+﻿using Euclid.Framework.Cqrs.NHibernate;
+using NHibernate;
+using StorefrontAgent.ReadModels;
+
+namespace StorefrontAgent.Queries
+{
+	public class CompanyQueries : NhQuery<Company>
+	{
+		public CompanyQueries(ISession session)
+			: base(session)
+		{
+		}
+	}
+}
