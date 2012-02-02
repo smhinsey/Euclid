a8be80697aff677827b57a1e462ca4af98181974
Shawn Hinsey <smhinsey@gmail.com>
2012-02-01 19:22:14 -0500

    fake queries for prototyping

--- apps/StorefrontAgent/Queries/CompanyQueries.cs
+++ apps/StorefrontAgent/Queries/CompanyQueries.cs
@@ -1,4 +1,6 @@
-﻿using Euclid.Framework.Cqrs.NHibernate;
+﻿using System;
+using System.Collections.Generic;
+using Euclid.Framework.Cqrs.NHibernate;
 using NHibernate;
 using StorefrontAgent.ReadModels;
 
@@ -9,6 +11,47 @@ namespace StorefrontAgent.Queries
 		public CompanyQueries(ISession session)
 			: base(session)
 		{
+		}
+
+		public IList<CompanyEmployee> FindEmployeesByCompany()
+		{
+			var results = new List<CompanyEmployee>
+				{
+					new CompanyEmployee
+						{
+							FirstName = "John",
+							LastName = "Doe",
+							HireDate = DateTime.Now.AddMonths(-5),
+							Location = "Atlanta",
+							Type = EmployeeType.Admin
+						},
+					new CompanyEmployee
+						{
+							FirstName = "Jane",
+							LastName = "Doe",
+							HireDate = DateTime.Now.AddMonths(-4),
+							Location = "Boston",
+							Type = EmployeeType.Admin
+						},
+					new CompanyEmployee
+						{
+							FirstName = "Jack",
+							LastName = "Doe",
+							HireDate = DateTime.Now.AddMonths(-19),
+							Location = "Charlotte",
+							Type = EmployeeType.StoreManagement
+						},
+					new CompanyEmployee
+						{
+							FirstName = "Jill",
+							LastName = "Doe",
+							HireDate = DateTime.Now.AddMonths(-9),
+							Location = "D.C.",
+							Type = EmployeeType.StoreManagement
+						},
+				};
+
+			return results;
 		}
 	}
 }
