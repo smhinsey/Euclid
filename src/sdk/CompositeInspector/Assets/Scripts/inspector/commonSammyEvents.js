759d8f76364c1f00fba80a1c6b6e804f4068a895
Shawn Hinsey <smhinsey@gmail.com>
2012-02-23 18:05:39 -0500

    introduced sammy as a quick prototype tool for the new inspector

---
+++ src/sdk/CompositeInspector/Assets/Scripts/inspector/commonSammyEvents.js
@@ -0,0 +1,59 @@
+﻿; (function ($) {
+
+	$.sammy(function () {
+
+		this.bind('render-model', function (e, data) {
+
+			var templateName = data['templateName'];
+
+			var model = data['model'];
+
+			var targetSelector = data['targetSelector'];
+
+			console.log("render-model rendering using template " + templateName);
+			console.log(model);
+
+			var templatePath = '/composite/js/inspector/templates/' + templateName + '.html';
+
+			console.log(templatePath);
+
+			fetchHandlebarsTemplate(templatePath, function (template) {
+				var renderedOutput = template(model);
+				$(targetSelector).html(renderedOutput);
+			});
+
+		});
+
+		this.bind('highlight-nav', function (e, data) {
+			var navSelector = "#nav-" + data['slug'] + data['current'];
+
+			$(".nav-list li").removeClass("active");
+
+			$(navSelector).addClass("active");
+		});
+
+	});
+
+	function fetchHandlebarsTemplate(path, callback) {
+		var source;
+		var template;
+
+		console.log("Loading handlebars template from " + path);
+
+		$.ajax({
+			url: path,
+			success: function (data) {
+
+				data = "<script>" + data + "</script>";
+
+				source = $(data).html();
+
+				template = Handlebars.compile(source);
+
+				if (callback) {
+					callback(template);
+				};
+			}
+		});
+	}
+})(jQuery);
---
+++ src/sdk/CompositeInspector/Assets/Scripts/inspector/inspector.js
@@ -0,0 +1,29 @@
+﻿$.getScript("/composite/js/inspector/commonSammyEvents.js");
+
+
+; (function ($) {
+	var app = $.sammy(function () {
+
+		this.get('/composite/new/#agents', function () {
+
+			var context = this;
+
+			WorkWithDataFromUrl("/composite/api", function (compositeMetadata) {
+				context.trigger('render-model', {templateName: 'globalNav', model: compositeMetadata, targetSelector: "#inspectorGlobalNav"});
+				context.trigger('render-model', {templateName: 'home', model: compositeMetadata, targetSelector: "#inspectorMain"});
+				context.trigger('render-model', {templateName: 'description', model: compositeMetadata, targetSelector: "#inspectorDescription"});
+			});
+
+			this.trigger('highlight-nav', {slug: 'SysAdmin', current: 'Dashboard'});
+		});
+
+		this.get('/composite/new', function () {
+			this.redirect('/composite/new/#agents');
+		});
+
+	});
+	
+	$(function () {
+		app.run();
+	});
+})(jQuery);
