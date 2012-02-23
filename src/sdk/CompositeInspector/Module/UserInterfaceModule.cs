040f27ee768d2f646e25e596abf36dc6bae4e208
logicbomb <jason.turim@gmail.com>
2012-02-22 23:25:03 -0500

    remove unused routes

--- src/sdk/CompositeInspector/Module/UserInterfaceModule.cs
+++ src/sdk/CompositeInspector/Module/UserInterfaceModule.cs
@@ -14,29 +14,12 @@ namespace CompositeInspector.Module
 		private const string HomeRoute = "/inspector";
 		private const string HomeViewPath = "inspector";
 
-		private const string AgentDetailRoute = "/agent/{agentSystemName}";
-		private const string AgentDetailView = "view-agent.cshtml";
-		
-		private const string CompositeDetailRoute = "/details";
-
-		private const string CommandRegistryRoute = "/command-registry";
-		private const string CommandRegistryView = "view-command-registry";
-
-		private const string LogsRoute = "/Logs";
-		private const string LogsView = "view-logs";
-		
 		public UserInterfaceModule()
 			: base(BaseRoute)
 		{
 			Get[IndexRoute] = _ => View[HomeViewPath];
 
 			Get[HomeRoute] = _ => View[HomeViewPath];
-
-			Get[AgentDetailRoute] = p => View[AgentDetailView];
-
-			Get[CommandRegistryRoute] = _ => View[CommandRegistryView];
-
-			Get[CompositeDetailRoute] = _ => Response.AsRedirect("/composite/", RedirectResponse.RedirectType.Permanent);
 		}
 	}
 }
