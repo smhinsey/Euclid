9b94c8a1ab0b54c7e8fbf994e1144c7743e04bd3
Shawn Hinsey <smhinsey@gmail.com>
2012-02-24 03:29:19 -0500

    introduced notion of a composite explorer

--- src/sdk/CompositeInspector/Assets/Scripts/inspector/inspector.js
+++ src/sdk/CompositeInspector/Assets/Scripts/inspector/inspector.js
@@ -4,13 +4,13 @@ $.getScript("/composite/js/inspector/commonSammyEvents.js");
 ; (function ($) {
 	var app = $.sammy(function () {
 
-		this.get('/composite/new/#agents', function () {
+		this.get('/composite/new/#composite-explorer', function () {
 
 			var eventContext = this;
 			
 			WorkWithDataFromUrl("/composite/api", function (compositeMetadata) {
 				eventContext.trigger('render-model', {templateName: 'globalNav', model: compositeMetadata, targetSelector: "#inspectorGlobalNav"});
-				eventContext.trigger('render-model', {templateName: 'home', model: compositeMetadata, targetSelector: "#inspectorMain"});
+				eventContext.trigger('render-model', {templateName: 'compositeExplorer', model: compositeMetadata, targetSelector: "#inspectorMain"});
 				eventContext.trigger('render-model', {templateName: 'description', model: compositeMetadata, targetSelector: "#inspectorDescription"});
 			});
 			
@@ -33,7 +33,7 @@ $.getScript("/composite/js/inspector/commonSammyEvents.js");
 		});
 
 		this.get('/composite/new', function () {
-			this.redirect('/composite/new/#agents');
+			this.redirect('/composite/new/#composite-explorer');
 		});
 
 	});
