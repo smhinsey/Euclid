83d0205e5a7927afd7fc5ba4004bde9d1447f055
Shawn Hinsey <smhinsey@gmail.com>
2012-02-24 17:35:06 -0500

    in progress with new inspector ui

--- src/sdk/CompositeInspector/Assets/Scripts/inspector/inspector.js
+++ src/sdk/CompositeInspector/Assets/Scripts/inspector/inspector.js
@@ -4,16 +4,41 @@ $.getScript("/composite/js/inspector/commonSammyEvents.js");
 ; (function ($) {
 	var app = $.sammy(function () {
 
-		this.get('/composite/new/#composite-explorer', function () {
+		this.get('/composite/new/#explorer', function () {
 
 			var eventContext = this;
 			
 			WorkWithDataFromUrl("/composite/api", function (compositeMetadata) {
 				eventContext.trigger('render-model', {templateName: 'globalNav', model: compositeMetadata, targetSelector: "#inspectorGlobalNav"});
-				eventContext.trigger('render-model', {templateName: 'compositeExplorer', model: compositeMetadata, targetSelector: "#inspectorMain"});
+				eventContext.trigger('render-model', {templateName: 'explorer', model: compositeMetadata, targetSelector: "#inspectorMain"});
 				eventContext.trigger('render-model', {templateName: 'description', model: compositeMetadata, targetSelector: "#inspectorDescription"});
 			});
 			
+			this.trigger('highlight-nav', { current: 'Agents'});
+
+		});
+		
+		this.get('/composite/new/#explorer/agent/:agentSystemName', function () {
+
+			var eventContext = this;
+			
+			WorkWithDataFromUrl("/composite/api", function (compositeMetadata) {
+				eventContext.trigger('render-model', {templateName: 'globalNav', model: compositeMetadata, targetSelector: "#inspectorGlobalNav"});
+				eventContext.trigger('render-model', {templateName: 'explorer', model: compositeMetadata, targetSelector: "#inspectorMain"});
+				eventContext.trigger('render-model', {templateName: 'description', model: compositeMetadata, targetSelector: "#inspectorDescription"});
+			});
+			
+			WorkWithDataFromUrl("/composite/api/agent/" + data['agentSystemName'], function (agentMetadata) {
+				var commandRenderData = { ListClass: "dropdown-menu", Commands: agentMetadata.Commands };
+				Using(commandRenderData).Render("/composite/ui/template/commands").ReplaceContentsOf("#agent-commands");
+
+				var queryRenderData = { ListClass: "dropdown-menu", Queries: agentMetadata.Queries };
+				Using(queryRenderData).Render("/composite/ui/template/queries").ReplaceContentsOf("#agent-queries");
+
+				var readModelRenderData = { ListClass: "dropdown-menu", SystemName: agentMetadata.SystemName, ReadModels: agentMetadata.ReadModels};
+				Using(readModelRenderData).Render("/composite/ui/template/read-models").ReplaceContentsOf("#agent-read-models");
+			});
+
 			this.trigger('highlight-nav', { current: 'Agents'});
 
 		});
@@ -33,7 +58,7 @@ $.getScript("/composite/js/inspector/commonSammyEvents.js");
 		});
 
 		this.get('/composite/new', function () {
-			this.redirect('/composite/new/#composite-explorer');
+			this.redirect('/composite/new/#explorer');
 		});
 
 	});
